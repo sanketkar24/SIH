4aa23c0d41e9a5412dd4a60db23a9e9a9d00f0e9
Sanket Kar <sanketkar@Sankets-MacBook-Pro.local>
2023-08-21 15:04:01 +0530

    added custom font bold/regular/light in layout.js

---
+++ app/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footerText'> Find Your Brand's Perfect Shelf!</div>
+        <div className='footerTextHeading'> Company</div>
+        <div className='footerTextHeading'> Contact Us</div>
+        <div className='footerTextHeading'> Follow Us On</div>
+    </div>
+  )
+}
+export default Footer
