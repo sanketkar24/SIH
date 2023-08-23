5e9def4e87f21d78664c213ecae46408266b457b
Sanket Kar <sanketkar@Sankets-MacBook-Pro.local>
2023-08-23 16:34:55 +0530

    footer added and custom font now from global.css

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -3,10 +3,49 @@ import './index.css'
 function Footer() {
   return (
     <div className='footer'>
-        <div className='footerText'> Find Your Brand's Perfect Shelf!</div>
-        <div className='footerTextHeading'> Company</div>
-        <div className='footerTextHeading'> Contact Us</div>
-        <div className='footerTextHeading'> Follow Us On</div>
+      <div className='footerDivision'>
+        <div className='footerTextHeading'>
+          Find Your
+          <br />
+          Brand's Perfect
+          <br />
+          Shelf!
+        </div>
+        <div className='footerTextSubheading'>
+          Search • Find • Elevate
+        </div>
+      </div>
+      <div className='footerDivision'>
+        <div className='footerTextHeading'>
+          Company
+        </div>
+        <div className='footerTextSubheading'>
+          About Us
+          <br />
+          Terms and Conditions
+          <br />
+          Privacy Policy
+          <br />
+          FAQ
+        </div>
+      </div>
+      <div className='footerDivision'>
+        <div className='footerTextHeading'>
+          Contact Us
+        </div>
+        <div className='footerTextSubheading'>
+          connect@baylink.in
+          <br />
+          Innovation center, MIT Manipal
+          <br />
+          +91 7838687511
+        </div>
+      </div>
+      <div className='footerDivision'>
+        <div className='footerTextHeading'>
+          Follow Us On
+        </div>
+      </div>
     </div>
   )
 }
