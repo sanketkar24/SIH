b4ba35336416bbd9033d1f7ed8f005c1716423eb
Sandeep Kumar Rai <raisandeep.dev@gmail.com>
2023-08-25 18:12:00 +0530

    Removed style form js file and added a seperate css file

--- app/login_signup/signup.js
+++ app/login_signup/signup.js
@@ -9,6 +9,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { SiFacebook } from "react-icons/si";
+import "./signup.css"
+
 
 const Signup = (props) => {
   const [formData, setFormData] = useState({
@@ -30,7 +32,10 @@ const Signup = (props) => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
+    <div
+      className="flex justify-center items-center h-screen w-screen"
+      style={{ fontFamily: "General Sans" }}
+    >
       <div className="flex w-full h-full  bg-white">
         <div className="sm:hidden md:block flex-1 relative h-screen">
           <div className="relative h-full">
@@ -110,44 +115,19 @@ const Signup = (props) => {
         <div className="flex-1 flex flex-col justify-center items-center p-8">
           <div className="mt-1/2 w-2/3 ">
             <div
-              style={{
-                color:
-                  "var(--colour-pallete-primary-p-500, var(--colour-pallete-primary-p-500, #0E87D1))",
-                fontFamily: "General Sans",
-                fontSize: "18px",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "150%" /* 27px */,
-              }}
+              // className={`${generalSansBold.variable} ${generalSansRegular.variable} ${generalSansLight.variable}`}
+              id="heading-2"
+              style={{ fontFamily: "General Sans" }}
             >
               REGISTER
             </div>
-            <div className="flex">
+            <div className="flex ">
+              <div className="heading-1">Welcome to&nbsp;</div>
               <div
                 style={{
-                  color:
-                    "var(--colour-pallete-neutrals-n-900, var(--colour-pallete-neutrals-n-900, #000))",
-                  fontFamily: "General Sans",
-                  fontSize: "40px",
-                  fontStyle: "normal",
-                  fontWeight: 600,
-                  lineHeight: "125%" /* 50px */,
-                  marginTop: "20px",
+                  color: "#22B2E6",
                 }}
-              >
-                Welcome to&nbsp;
-              </div>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-neutrals-n-900, var(--colour-pallete-neutrals-n-900, #22B2E6))",
-                  fontFamily: "General Sans",
-                  fontSize: "40px",
-                  fontStyle: "normal",
-                  fontWeight: 600,
-                  lineHeight: "125%" /* 50px */,
-                  marginTop: "20px",
-                }}
+                className="heading-1"
               >
                 Baylink!
               </div>
@@ -155,181 +135,71 @@ const Signup = (props) => {
             <div className="text-secondary-2-s-200 font-GeneralSans text-base font-normal leading-6 mt-6">
               Register using your email or mobile number
             </div>
-            <div style={{ marginTop: "20px" }}>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                  fontFamily: "General Sans",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 24px */,
-                }}
-              >
-                Brand Name*
-              </div>
+            <div className="mt-4">
+              <div className="input-heading">Brand Name*</div>
               <input
                 type="text"
-                className="border border-gray-300 p-2 rounded-lg w-full"
+                className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                 placeholder="Enter store name"
-                style={{ marginTop: "10px" }}
+                
               />
             </div>
             <div className="flex justify-between">
               <div className="w-1/2 mr-4" style={{ marginTop: "20px" }}>
-                <div
-                  style={{
-                    color:
-                      "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                    fontFamily: "General Sans",
-                    fontSize: "16px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "150%" /* 24px */,
-                  }}
-                >
-                  First Name*
-                </div>
+                <div className="input-heading">First Name*</div>
                 <input
                   type="text"
-                  className="border border-gray-300 p-2 rounded-lg w-full"
+                  className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                   placeholder="Enter first name"
-                  style={{ marginTop: "10px" }}
                 />
               </div>
               <div className="w-1/2" style={{ marginTop: "20px" }}>
-                <div
-                  style={{
-                    color:
-                      "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                    fontFamily: "General Sans",
-                    fontSize: "16px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "150%" /* 24px */,
-                  }}
-                >
-                  Last Name*
-                </div>
+                <div className="input-heading">Last Name*</div>
                 <input
                   type="text"
-                  className="border border-gray-300 p-2 rounded-lg w-full"
+                  className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                   placeholder="Enter last name"
-                  style={{ marginTop: "10px" }}
                 />
               </div>
             </div>
             <div style={{ marginTop: "20px" }}>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                  fontFamily: "General Sans",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 24px */,
-                }}
-              >
-                Email or mobile number*
-              </div>
+              <div className="input-heading">Email or mobile number*</div>
               <input
                 type="text"
-                className="border border-gray-300 p-2 rounded-lg w-full"
+                className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                 placeholder="Enter your Email or mobile number"
-                style={{ marginTop: "10px" }}
               />
             </div>
             <div style={{ marginTop: "20px" }}>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                  fontFamily: "General Sans",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 24px */,
-                }}
-              >
-                Password*
-              </div>
+              <div className="input-heading">Password*</div>
               <input
                 type="password"
-                className="border border-gray-300 p-2 rounded-lg w-full"
+                className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                 placeholder="Enter your Password"
-                style={{ marginTop: "10px" }}
               />
             </div>
-            
+
             <div className="flex items-center mt-4">
-                <input type="checkbox" id="terms-checkbox" className="mr-2" />
-                <label
-                    htmlFor="terms-checkbox"
-                    className="text-sm font-normal leading-6"
-                    style={{
-                        color: "var(--colour-pallete-secondary-2-s-200, var(--colour-pallete-secondary-2-s-200, #383839))",
-                        fontFamily: "General Sans",
-                        fontSize: "16px",
-                        fontStyle: "normal",
-                        fontWeight: 400,
-                        lineHeight: "150%" /* 24px */
-                    }}
-                >
-                    I agree to the <a href="#">Terms</a> and{" "}
-                    <a href="#">Privacy Policy</a>
-                </label>
+              <input type="checkbox" id="terms-checkbox" className="mr-2" />
+              <label
+                htmlFor="terms-checkbox"
+                className="text-sm font-normal leading-6 input-heading"
+              >
+                I agree to the <a href="#">Terms</a> and{" "}
+                <a href="#">Privacy Policy</a>
+              </label>
             </div>
             <div className="flex justify-center mt-4">
-              <button
-                className="bg-blue-500 text-white p-2 rounded-lg w-full border-2 border-blue-300 hover:bg-blue-700 hover:border-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
-                style={{
-                  borderRadius: "5px",
-                  background: "var(--colour-pallete-primary-p-300, #22B2E6)",
-                  boxShadow: "0px 2px 0px 0px rgba(0, 0, 0, 0.04)",
-                }}
-              >
+              <button className="bg-blue-500 text-white p-2 rounded-lg w-full border-2 border-blue-300 hover:bg-blue-700 hover:border-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300">
                 Register
               </button>
             </div>
-            <div
-              className="flex justify-center mt-4"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginTop: "20px",
-              }}
-            >
-              <hr
-                style={{
-                  width: "100%",
-                  borderTop: "1px solid #B0B0B0",
-                  margin: "0px",
-                }}
-              />
-            <div 
-                style={{ 
-                    margin: "0px 10px",
-                    color: "var(--colour-pallete-neutrals-n-60, var(--colour-pallete-neutrals-n-60, #B0B0B0))",
-                    textAlign: "center",
-                    fontFamily: "General Sans",
-                    fontSize: "14px",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "125%" /* 17.5px */
-                }}
-            >
+            <div className="flex justify-center mt-4 items-center">
+              <hr className="w-full border-t border-gray-300 my-0" />
+              <div className="mx-10 text-gray-500 text-center font-GeneralSans text-sm font-normal leading-5">
                 Or
-            </div>
-              <hr
-                style={{
-                  width: "100%",
-                  borderTop: "1px solid #B0B0B0",
-                  margin: "0px",
-                  //   background: "var(--colour-pallete-neutrals-n-60, #)",
-                }}
-              />
+              </div>
+              <hr className="w-full border-t border-gray-300 my-0" />
             </div>
             <div className="flex w-full" style={{ marginTop: "20px" }}>
               {/* import { FaGoogle } from 'react-icons/fa'; */}
@@ -345,36 +215,13 @@ const Signup = (props) => {
               </button>
             </div>
             <div
-              className="flex w-full justify-center items-center"
-              style={{ marginTop: "20px" }}
+              className="flex w-full justify-center items-center mt-4"
             >
-              <div
-                className="flex justify-center mt-4"
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-200, var(--colour-pallete-secondary-2-s-200, #383839))",
-                  textAlign: "center",
-                  fontFamily: "General Sans",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 21px */,
-                }}
-              >
+                <div className="flex justify-center mt-4 text-gray-500 text-center font-GeneralSans text-sm font-normal leading-5">
                 <div>Already have an account?&nbsp;</div>
               </div>
               <div
-                className="flex justify-center items-center mt-4"
-                style={{
-                  color:
-                    "var(--colour-pallete-primary-p-500, var(--colour-pallete-primary-p-500, #0E87D1))",
-                  fontFamily: "General Sans",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: 500,
-                  lineHeight: "150%",
-                  marginTop: "20px",
-                }}
+                className="flex justify-center items-center mt-4 text-primary-p-500 font-GeneralSans font-medium text-sm blue-color"
               >
                 Login
               </div>
