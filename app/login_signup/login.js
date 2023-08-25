9de800528e70b883b3038a64435354e8ef245e7b
Sandeep Kumar Rai <raisandeep.dev@gmail.com>
2023-08-25 18:42:44 +0530

    Removed inline css

--- app/login_signup/login.js
+++ app/login_signup/login.js
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import {SiFacebook} from "react-icons/si";
+import './login.css'
 
 const Signup = (props) => {
   const [formData, setFormData] = useState({
@@ -28,10 +29,14 @@ const Signup = (props) => {
     event.preventDefault();
     console.log(formData);
   };
-  console.log('Hellooooooooooooo',props.check);
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
+    <div
+      className="flex justify-center items-center h-screen w-screen"
+      style={{
+        fontFamily: "General Sans",
+      }}
+    >
       <div className="flex w-full h-full  bg-white">
         <div className="sm:hidden md:block flex-1 relative h-screen">
           <div className="relative h-full">
@@ -42,59 +47,26 @@ const Signup = (props) => {
               objectFit="cover"
             />
             <div className="absolute top-0 left-0 p-4">
-              <Image src={props.check=="Brand"?Logo:BlueLogo} alt="Logo" />
+              <Image
+                src={props.check == "Brand" ? Logo : BlueLogo}
+                alt="Logo"
+              />
             </div>
             <div className="absolute bottom-10 left-10 p-4">
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-1-s-100, var(--colour-pallete-secondary-1-s-100, #F6F6F6))",
-                  fontFamily: "General Sans",
-                  fontSize: "40px",
-                  fontStyle: "normal",
-                  fontWeight: 600,
-                  lineHeight: "125%" /* 50px */,
-                }}
-              >
+              <div className="text-gray-100 font-bold text-4xl">
                 Explore your Favourite
               </div>
               <div className="md:flex">
-                {props.check=="Brand"?<div
-                  style={{
-                    color:
-                      "var(--colour-pallete-secondary-1-s-100, var(--colour-pallete-secondary-1-s-100, #F6F6F6))",
-                    fontFamily: "General Sans",
-                    fontSize: "40px",
-                    fontStyle: "normal",
-                    fontWeight: 600,
-                    lineHeight: "125%" /* 50px */,
-                  }}
-                >
-                  Brands on
-                </div>:<div
-                  style={{
-                    color:
-                      "var(--colour-pallete-secondary-1-s-100, var(--colour-pallete-secondary-1-s-100, #F6F6F6))",
-                    fontFamily: "General Sans",
-                    fontSize: "40px",
-                    fontStyle: "normal",
-                    fontWeight: 600,
-                    lineHeight: "125%" /* 50px */,
-                  }}
-                >
-                  Stores on
-                </div>}
-                <div
-                  style={{
-                    color:
-                      "var(--colour-pallete-primary-p-300, var(--colour-pallete-primary-p-300, #22B2E6))",
-                    fontFamily: "General Sans",
-                    fontSize: "40px",
-                    fontStyle: "normal",
-                    fontWeight: 600,
-                    lineHeight: "125%",
-                  }}
-                >
+                {props.check == "Brand" ? (
+                  <div className="text-gray-100 font-semibold text-4xl">
+                    Brands on
+                  </div>
+                ) : (
+                  <div className="text-white font-semibold text-4xl">
+                    Stores on
+                  </div>
+                )}
+                <div className="text-primary-p-300 font-bold text-4xl blue-brand">
                   &nbsp;Baylink!
                 </div>
               </div>
@@ -103,79 +75,33 @@ const Signup = (props) => {
         </div>
         <div className="flex-1 flex flex-col justify-center items-center p-8">
           <div className="mt-1/2 w-2/3 ">
-            <div
-              style={{
-                color:
-                  "var(--colour-pallete-primary-p-500, var(--colour-pallete-primary-p-500, #0E87D1))",
-                fontFamily: "General Sans",
-                fontSize: "18px",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "150%" /* 27px */,
-              }}
-            >
+            <div className="text-primary-p-500 font-medium text-lg blue-text">
               LOGIN
             </div>
-            <div
-              style={{
-                color:
-                  "var(--colour-pallete-neutrals-n-900, var(--colour-pallete-neutrals-n-900, #000))",
-                fontFamily: "General Sans",
-                fontSize: "40px",
-                fontStyle: "normal",
-                fontWeight: 600,
-                lineHeight: "125%" /* 50px */,
-                marginTop: "20px",
-              }}
-            >
+            <div className="text-black font-semibold text-4xl my-5">
               Welcome Back!
             </div>
-            <div style={{ marginTop: "20px" }}>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                  fontFamily: "General Sans",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 24px */,
-                }}
-              >
+            <div className="mt-4">
+              <div className="text-gray-700 font-normal text-base">
                 Email or mobile number
               </div>
               <input
                 type="text"
-                className="border border-gray-300 p-2 rounded-lg w-full"
+                className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                 placeholder="Enter your Email or mobile number"
-                style={{ marginTop: "10px" }}
               />
             </div>
-            <div style={{ marginTop: "20px" }}>
-              <div
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-300, var(--colour-pallete-secondary-2-s-300, #0F0F10))",
-                  fontFamily: "General Sans",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 24px */,
-                }}
-              >
+            <div className="mt-4">
+              <div className="text-secondary-2-s-300 font-general-sans text-base font-normal leading-6">
                 Password
               </div>
               <input
                 type="password"
-                className="border border-gray-300 p-2 rounded-lg w-full"
+                className="border border-gray-300 p-2 rounded-lg w-full mt-2"
                 placeholder="Enter your Password"
-                style={{ marginTop: "10px" }}
               />
             </div>
-            <div
-              className="flex justify-between w-full"
-              style={{ marginTop: "10px" }}
-            >
+            <div className="flex justify-between w-full mt-2">
               <div className="w-full text-right text-primary-p-500 font-general-sans text-sm font-normal leading-6">
                 <a href="#" className="hover:underline text-blue-500">
                   Forgot Password?
@@ -184,12 +110,8 @@ const Signup = (props) => {
             </div>
             <div className="flex justify-center mt-4">
               <button
-                className="bg-blue-500 text-white p-2 rounded-lg w-full border-2 border-blue-300 hover:bg-blue-700 hover:border-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
-                style={{
-                  borderRadius: "5px",
-                  background: "var(--colour-pallete-primary-p-300, #22B2E6)",
-                  boxShadow: "0px 2px 0px 0px rgba(0, 0, 0, 0.04)",
-                }}
+                className="blue-background text-white p-2 rounded-lg w-full border-2 border-blue-300 hover:bg-blue-700 hover:border-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition-colors duration-300"
+               
               >
                 Sign In
               </button>
@@ -202,26 +124,11 @@ const Signup = (props) => {
                 marginTop: "20px",
               }}
             >
-              <hr
-                style={{
-                  width: "100%",
-                  borderTop: "1px solid #B0B0B0",
-                  margin: "0px",
-                }}
-              />
-              <div style={{ margin: "0px 10px" }}>Or</div>
-              <hr
-                style={{
-                  width: "100%",
-                  borderTop: "1px solid #B0B0B0",
-                  margin: "0px",
-                  //   background: "var(--colour-pallete-neutrals-n-60, #)",
-                }}
-              />
+              <hr className="horizontal-rular" />
+              <div className="mx-2 ">Or</div>
+              <hr className="horizontal-rular" />
             </div>
-            <div className="flex w-full" style={{ marginTop: "20px" }}>
-              {/* import { FaGoogle } from 'react-icons/fa'; */}
-
+            <div className="flex w-full mt-4">
               <button className="mr-10 border border-gray-300 p-2 rounded-lg w-full items-center justify-center">
                 <FcGoogle size={24} className="inline-block mr-2" />
                 Google
@@ -232,38 +139,11 @@ const Signup = (props) => {
                 facebook
               </button>
             </div>
-            <div
-              className="flex w-full justify-center items-center"
-              style={{ marginTop: "20px" }}
-            >
-              <div
-                className="flex justify-center mt-4"
-                style={{
-                  color:
-                    "var(--colour-pallete-secondary-2-s-200, var(--colour-pallete-secondary-2-s-200, #383839))",
-                  textAlign: "center",
-                  fontFamily: "General Sans",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%" /* 21px */,
-                }}
-              >
+            <div className="flex w-full justify-center items-center mt-4">
+              <div className="flex justify-center mt-4 text-secondary-2-s-200 font-general-sans text-base font-normal leading-6 text-center">
                 <div>Don't have an account?&nbsp;</div>
               </div>
-              <div
-                className="flex justify-center items-center mt-4"
-                style={{
-                  color:
-                    "var(--colour-pallete-primary-p-500, var(--colour-pallete-primary-p-500, #0E87D1))",
-                  fontFamily: "General Sans",
-                  fontSize: "14px",
-                  fontStyle: "normal",
-                  fontWeight: 500,
-                  lineHeight: "150%",
-                  marginTop: "20px",
-                }}
-              >
+              <div className="flex justify-center items-center mt-4 text-primary-p-500 font-general-sans text-sm font-medium leading-6 blue-text">
                 Sign Up
               </div>
             </div>
